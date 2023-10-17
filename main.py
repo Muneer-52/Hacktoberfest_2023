fcce173c48ffe31f7f9844fd2e6d745df8f1d086
Muneer-52 <116058724+Muneer-52@users.noreply.github.com>
2023-10-17 04:13:24 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+
 print("Hello Pthon") 
+
+print("Hello Phycharm")
+print("Hello World")
+print("hello Colab")
+x = 5
+y = "John"
+print(x)
+print(y)
+
+name = input('What is your name?\n')     # \n ---> newline  ---> It causes a line break
+print(name)
+
+fruits = ["apple", "banana", "cherry"]
+for a in fruits:
+  print(a)
+  if a == "banana":
+    break
+cars = ["Ford", "Volvo", "BMW"]
+
